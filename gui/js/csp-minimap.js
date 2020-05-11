d72a3e880ee743c1ac17e067810dc3bea9697c87
Simon Schneegans <code@simonschneegans.de>
2020-05-11 10:09:06 +0200

    :tada: Add controls to minimap

--- gui/js/csp-minimap.js
+++ gui/js/csp-minimap.js
@@ -13,6 +13,89 @@
 
     _lastLng = 0;
     _lastLat = 0;
+    _locked  = true;
+
+    _customControls = L.Control.extend({
+      options: {position: 'topleft'},
+      onAdd:
+          () => {
+            let createButton = (icon, tooltip, callback) => {
+              var button       = L.DomUtil.create('a');
+              button.innerHTML = `<i class="material-icons">${icon}</i>`;
+              button.setAttribute('title', tooltip);
+              button.dataset.toggle    = 'tooltip';
+              button.dataset.placement = 'left';
+              L.DomEvent.on(button, 'click', callback);
+
+              // Don't let mouse events get to the map.
+              L.DomEvent.on(button, 'dblclick', L.DomEvent.stop);
+              L.DomEvent.on(button, 'mousedown', L.DomEvent.stop);
+              L.DomEvent.on(button, 'mouseup', L.DomEvent.stop);
+
+              return button;
+            };
+
+            let zoomInButton = createButton('add', 'Zoom in', (e) => {
+              this._minimap.zoomIn();
+              L.DomEvent.stop(e);
+            });
+
+            let zoomOutButton = createButton('remove', 'Zoom out', (e) => {
+              this._minimap.zoomOut();
+              L.DomEvent.stop(e);
+            });
+
+            let zoomResetButton = createButton('zoom_out_map', 'Fit map', (e) => {
+              this._minimap.setView([0, 0], 0);
+              L.DomEvent.stop(e);
+            });
+
+            let lockButton = createButton('lock', 'Lock Minimap to Observer', (e) => {
+              this._locked = !this._locked;
+              if (this._locked) {
+                e.target.querySelector('i').textContent = 'lock';
+              } else {
+                e.target.querySelector('i').textContent = 'lock_open';
+              }
+
+              L.DomEvent.stop(e);
+            });
+
+            let centerButton = createButton('my_location', 'Center Minimap on Observer', (e) => {
+              this._centerMapOnObserver(true);
+              L.DomEvent.stop(e);
+            });
+
+            let bookmarkButton = createButton('bookmark', 'Add a Bookmark', (e) => {
+              CosmoScout.bookmarkEditor.addNewBookmark();
+              L.DomEvent.stop(e);
+            });
+
+            let container = L.DomUtil.create('div');
+
+            let groupA = L.DomUtil.create('div');
+            groupA.classList.add('leaflet-bar')
+            groupA.appendChild(zoomInButton);
+            groupA.appendChild(zoomOutButton);
+            groupA.appendChild(zoomResetButton);
+            container.appendChild(groupA);
+
+            let groupB = L.DomUtil.create('div');
+            groupB.classList.add('leaflet-bar')
+            groupB.style.marginTop = "10px";
+            groupB.appendChild(centerButton);
+            groupB.appendChild(lockButton);
+            container.appendChild(groupB);
+
+            let groupC = L.DomUtil.create('div');
+            groupC.classList.add('leaflet-bar')
+            groupC.style.marginTop = "10px";
+            groupC.appendChild(bookmarkButton);
+            container.appendChild(groupC);
+
+            return container;
+          }
+    });
 
     init() {
       // Add the minimap window.
@@ -21,14 +104,19 @@
 
       // Create the Leaflet map.
       this._minimap = L.map(document.querySelector('#minimap .window-content'), {
+        attributionControl: false,
+        zoomControl: false,
         center: [0, 0],
         zoom: 0,
         worldCopyJump: true,
-        maxBounds: [[-90, -180], [90, 180]],
-        attributionControl: false
+        maxBounds: [[-90, -180], [90, 180]]
       });
 
-      L.control.scale().addTo(this._minimap);
+      // Add our custom buttons.
+      this._minimap.addControl(new this._customControls());
+
+      // Add the attribution control to the bottom left.
+      L.control.attribution({position: 'bottomleft'}).addTo(this._minimap);
 
       // Create a marker for the user's position.
       this._observerMarker =
@@ -39,19 +127,17 @@
                           attribution: '&copy; OpenStreetMap contributors'
                         }).addTo(this._minimap);
 
-      // // Moving the planet with the minimap.
-      // this._minimap.on('click', (e) => {
-      //   // const location = {
-      //   //   longitude: parseFloat(e.latlng.lng),
-      //   //   latitude: parseFloat(e.latlng.lat),
-      //   //   height: this.circumference / Math.pow(2, this._minimap.getZoom())
-      //   // };
+      // Move the observer when clicked on the minimap.
+      this._minimap.on('click', (e) => {
+        let lng    = parseFloat(e.latlng.lng);
+        let lat    = parseFloat(e.latlng.lat);
+        let center = CosmoScout.state.activePlanetCenter;
+        let height = this._zoomToHeight(this._minimap.getZoom());
 
-      //   // if (this.activePlanet !== "") {
-      //   //   this.flyTo(this.activePlanet, location, 5);
-      //   // }
-      //   console.log("click");
-      // });
+        if (center !== "") {
+          CosmoScout.callbacks.navigation.setBodyLongLatHeightDuration(center, lng, lat, height, 5);
+        }
+      });
 
       // Resize the leaflet map on minimap window resize events.
       this._resizeObserver = new ResizeObserver((entries) => {
@@ -68,18 +154,38 @@
       this._resizeObserver.observe(this._minimapDiv);
     }
 
+    // Update minimap based on observer state.
     update() {
       // Do not do anything if invisible.
       if (this._minimapDiv.classList.contains("visible")) {
+        this._centerMapOnObserver(false);
+      }
+    }
+
+    // These are quite a crude conversions from the minimap zoom level to observer height. It
+    // works quite well for middle latitudes and the standard field of view.
+    _zoomToHeight(zoom) {
+      return 0.01 * this._minimapDiv.clientWidth * CosmoScout.state.activePlanetRadius[0] /
+             Math.pow(2, zoom);
+    }
+
+    _heightToZoom(height) {
+      return Math.log2(0.01 * this._minimapDiv.clientWidth *
+                       CosmoScout.state.activePlanetRadius[0] / Math.max(100, height));
+    }
+
+    // Update minimap based on observer state.
+    _centerMapOnObserver(force) {
+      if (CosmoScout.state.observerLngLatHeight) {
 
         // Update position of observer marker.
-        if (CosmoScout.state.observerLngLatHeight) {
+        let [lng, lat] = CosmoScout.state.observerLngLatHeight;
 
-          let [lng, lat] = CosmoScout.state.observerLngLatHeight;
-          this._observerMarker.setLatLng([lat, lng])
+        // Center the minimap around the observer.
+        if (force || (this._lastLng !== lng || this._lastLat !== lat)) {
+          this._observerMarker.setLatLng([lat, lng]);
 
-          // Center the minimap around the observer.
-          if ((this._lastLng !== lng || this._lastLat !== lat)) {
+          if (force || this._locked) {
             this._minimap.setView([lat, lng], [this._minimap.getZoom()]);
             this._lastLng = lng;
             this._lastLat = lat;
